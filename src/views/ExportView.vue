0a0c986fd2b03f21596e5636f6e7348f2021bb3c
Steven Boddez <stevenboddez@gmail.com>
2022-09-08 17:19:13 -0600

    Add warning for browser support of certain color spaces

--- src/views/ExportView.vue
+++ src/views/ExportView.vue
@@ -6,6 +6,20 @@
                 :options="options"
                 @select="onSelectFormat"
             />
+            <div
+                v-if="checkSupport"
+                class="support-warning"
+            >
+                <alert-triangle
+                    class="mr-2"
+                    color="rgb(153,27,27)"
+                />
+                check&nbsp;<a
+                    :href="checkSupport"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >browser support</a>&nbsp;before using this format
+            </div>
         </div>
         <export-text :content="coloursAsList" />
         <export-text :content="coloursAsArray" />
@@ -19,25 +33,31 @@ import ButtonGroup from '@/components/ButtonGroup/ButtonGroup.vue';
 import ExportText from '@/components/ExportText.vue';
 import useStore from '@/store';
 import type { ExportFormat } from '@/types';
+import { AlertTriangle } from 'lucide-vue-next';
 
 const store = useStore();
 const { setExportFormat } = store;
 const { exportFormat, coloursAsList, coloursAsArray } = storeToRefs(store);
 
-const modeOptions = [
+const formatOptions = [
     { label: 'hex', value: 'HEX' },
     { label: 'rgb', value: 'RGB' },
     { label: 'hsl', value: 'HSL' },
-    { label: 'hwb', value: 'HWB' },
-    { label: 'lab', value: 'LAB' }
+    { label: 'hwb', value: 'HWB', check: 'https://caniuse.com/mdn-css_types_color_hwb' },
+    { label: 'lab', value: 'LAB', check: 'https://caniuse.com/css-lch-lab' }
 ];
 
 const options = computed(
-    () => modeOptions.map(op => ({
+    () => formatOptions.map(op => ({
         ...op,
         selected: exportFormat.value === op.value
     }))
 );
+
+const checkSupport = computed(() => {
+    const formatOption = formatOptions.find(op => op.value === exportFormat.value);
+    return formatOption?.check;
+});
 
 const onSelectFormat = (format: string) => {
     setExportFormat(format as ExportFormat);
@@ -48,6 +68,14 @@ const onSelectFormat = (format: string) => {
 #export-view {
     .options {
         @apply flex justify-start gap-8 mb-4;
+
+        .support-warning {
+            @apply flex items-center font-sans text-red-800;
+
+            a {
+                @apply font-sans underline text-red-700 opacity-70;
+            }
+        }
     }
 }
 </style>
