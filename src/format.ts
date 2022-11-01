e7331701bf9b2d1145fdf600a8f941694a51e5a5
Steven Boddez <stevenboddez@gmail.com>
2022-11-01 15:16:37 -0600

    Fix export colour rounding errors

--- src/format.ts
+++ src/format.ts
@@ -2,6 +2,17 @@ import Color from 'color';
 import hsluv from 'hsluv';
 import type { ExportFormat, HSLuvColour } from '@/types';
 
+export function optionalZeros(n: number, maxDecimals = 3): string {
+    return (new Intl.NumberFormat(
+        'en-US',
+        {
+            style: 'decimal',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: maxDecimals
+        }
+    )).format(n);
+}
+
 /* RGB <-> HSLuv for colour generation */
 
 export function rgbToHsluv(hexOrRgb: string): HSLuvColour {
@@ -27,10 +38,16 @@ export function hsluvToRgb(parts: HSLuvColour): string {
 export const rgbToFormat: Record<ExportFormat, (rgb: string) => string> = {
     HEX: (rgb: string) => Color(rgb).hex(),
     RGB: (rgb: string) => rgb,
-    HSL: (rgb: string) => Color(rgb).hsl().string(),
-    HWB: (rgb: string) => Color(rgb).hwb().string(),
+    HSL: (rgb: string) => {
+        const c = Color(rgb).hsl();
+        return `hsl(${optionalZeros(c.hue())}, ${optionalZeros(c.saturationl())}%, ${optionalZeros(c.lightness())}%)`;
+    },
+    HWB: (rgb: string) => {
+        const c = Color(rgb).hwb();
+        return `hwb(${optionalZeros(c.hue())}, ${optionalZeros(c.white())}%, ${optionalZeros(c.black())}%)`;
+    },
     LAB: (rgb: string) => {
         const c = Color(rgb);
-        return `lab(${c.l().toFixed(3)}%, ${c.a().toFixed(3)}, ${c.b().toFixed(3)})`;
+        return `lab(${optionalZeros(c.l())}%, ${optionalZeros(c.a())}, ${optionalZeros(c.b())})`;
     }
 };
