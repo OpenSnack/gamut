b1383e41a47a72734fcd0420b5a8c5468a0c8e48
Steven Boddez <stevenboddez@gmail.com>
2022-11-01 17:34:38 -0600

    Plug in chroma.js for Delta E 2000

--- src/format.ts
+++ src/format.ts
@@ -1,5 +1,5 @@
 import Color from 'color';
-import hsluv from 'hsluv';
+import hsluv, { type ColorTuple } from 'hsluv';
 import type { ExportFormat, HSLuvColour } from '@/types';
 
 export function optionalZeros(n: number, maxDecimals = 3): string {
@@ -34,20 +34,35 @@ export function hsluvToRgb(parts: HSLuvColour): string {
 }
 
 /* RGB to other CSS-friendly formats */
+export function formatHSL(c: Color): string {
+    return `hsl(${optionalZeros(c.hue())}, ${optionalZeros(c.saturationl())}%, ${optionalZeros(c.lightness())}%)`;
+}
+
+export function formatHWB(c: Color): string {
+    return `hwb(${optionalZeros(c.hue())}, ${optionalZeros(c.white())}%, ${optionalZeros(c.black())}%)`;
+}
+
+export function formatLAB(c: Color): string {
+    return `lab(${optionalZeros(c.l())}%, ${optionalZeros(c.a())}, ${optionalZeros(c.b())})`;
+}
+
+export function formatLCHParts(c: ColorTuple): string {
+    return `lch(${optionalZeros(c[0])}%, ${optionalZeros(c[1])}, ${optionalZeros(c[2])})`;
+}
 
 export const rgbToFormat: Record<ExportFormat, (rgb: string) => string> = {
     HEX: (rgb: string) => Color(rgb).hex(),
     RGB: (rgb: string) => rgb,
     HSL: (rgb: string) => {
         const c = Color(rgb).hsl();
-        return `hsl(${optionalZeros(c.hue())}, ${optionalZeros(c.saturationl())}%, ${optionalZeros(c.lightness())}%)`;
+        return formatHSL(c);
     },
     HWB: (rgb: string) => {
         const c = Color(rgb).hwb();
-        return `hwb(${optionalZeros(c.hue())}, ${optionalZeros(c.white())}%, ${optionalZeros(c.black())}%)`;
+        return formatHWB(c);
     },
     LAB: (rgb: string) => {
         const c = Color(rgb);
-        return `lab(${optionalZeros(c.l())}%, ${optionalZeros(c.a())}, ${optionalZeros(c.b())})`;
+        return formatLAB(c);
     }
 };
